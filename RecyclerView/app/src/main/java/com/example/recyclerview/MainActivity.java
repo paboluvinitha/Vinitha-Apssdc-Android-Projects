10ff23e1ced98c0560fcbb265f3dc82c54271e28
unknown <vinithapabolu12@gmail.com>
2021-04-03 08:09:25 +0530

    recycle

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rec;
+    String names[];
+    String location[];
+    int poster[];
+    MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rec=findViewById(R.id.tv);
+        names=getResources().getStringArray(R.array.titles);
+        location=getResources().getStringArray(R.array.subtitles);
+        poster=new int[]{
+                R.drawable.ic_launcher_foreground,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_foreground,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_foreground,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_foreground,
+                R.drawable.ic_launcher_background};
+        adapter=new MyAdapter(this,names,location,poster);
+        rec.setLayoutManager(new LinearLayoutManager(this));
+        rec.setAdapter(adapter);
+
+    }
+}
